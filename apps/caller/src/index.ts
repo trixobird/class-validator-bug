dc11049f5c22d373bffa4598a61a40934602154e
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2025-02-10 17:18:40 +0200

    initial commit

---
+++ apps/caller/src/index.ts
@@ -0,0 +1,23 @@
+import { validate, validateOrReject, } from 'class-validator';
+import { Post } from '@repo/dto';
+
+let post = new Post();
+post.title = 'Hello'; // should not pass
+post.text = 'this is a great post about hell world'; // should not pass
+post.rating = 11; // should not pass
+post.email = 'google.com'; // should not pass
+post.site = 'googlecom'; // should not pass
+
+validate(post).then(errors => {
+  // errors is an array of validation errors
+  if (errors.length > 0) {
+    console.log('validation failed. errors: ', errors);
+  } else {
+    console.log('validation succeed');
+  }
+});
+
+validateOrReject(post).catch(errors => {
+  console.log('Promise rejected (validation failed). Errors: ', errors);
+});
+
